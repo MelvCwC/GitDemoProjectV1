0834fc6b44fcc3cb3d84ca8a1e63b4c63ea9c488
BowlOfOwls <weikang97@gmail.com>
2024-01-24 11:03:22 +0800

    First Commit

---
+++ src/defaultPackage/Test.java
@@ -0,0 +1,17 @@
+package defaultPackage;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "tired";
+		
+		if (str == "tired") {
+			System.out.println("Go rest!");
+		}
+		else {
+			System.out.println("Do work!");
+		}
+	}
+
+}
